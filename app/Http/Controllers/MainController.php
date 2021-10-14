d12413f81d01124b1252e230983fdad07ad8d963
4eda <4eda777@mail.ru>
2021-10-14 16:53:07 +0300

    Realus

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Product;
 use App\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('index');
+        $products = Product::get();
+        return view('index', compact('products'));
     }
 
 
--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -2,14 +2,11 @@
     <div class="thumbnail">
         <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
         <div class="caption">
-            <h3>iPhone X 64GB</h3>
-            <p>71990 ₽</p>
+            <h3>{{$product->name}}</h3>
+            <p>{{$product->price}}</p>
             <p>
                 <a href="{{route('basket')}}" class="btn btn-primary" role="button">В корзину</a>
-                @isset($category)
-                {{$category->name}}
-                @endisset
-                <a href="https://internet-shop.tmweb.ru/mobiles/iphone_x_64" class="btn btn-default" role="button">Подробнее</a>
+              <a href="{{ route('product', [$product->category->code , $product->code])}}" class="btn btn-default" role="button">Подробнее</a>
                 <input type="hidden" name="_token" value="0FEIUVKMrZ5ORLC5bs98u2bL9oXVRK8nlbF6yT3L"> 
             </p>
         </div>
